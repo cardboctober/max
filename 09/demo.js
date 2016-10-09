df36ccc82c18594543eb55c09f5afa2be3ea7b92
Max Glenister <moglenstar@gmail.com>
2016-10-09 14:52:46 +0100

    Added day 9!

---
+++ 09/demo.js
@@ -0,0 +1,370 @@
+(function(window, document) {
+  'use strict';
+  var core = window.core;
+  var T = window.THREE;
+
+  core.createFullScreenControl();
+
+  var renderer, scene, camera, effect, controls, ground, ambientLight, light, obj, micon;
+
+  scene = new T.Scene();
+  camera = core.setCameraOptions();
+  if (core.isPocketDevice()) {
+    camera.position.set(0, 20, 0);
+  } else {
+    camera.position.set(0, 60, 0);
+  }
+  var reticle = vreticle.Reticle(camera, .5);
+  scene.add(camera);
+
+  renderer = new T.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+    logarithmicDepthBuffer: true,
+  });
+
+  renderer.setSize(core.options.width, core.options.height);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.soft = true;
+  document.body.appendChild(renderer.domElement);
+  controls = core.setControllerMethod(camera, renderer.domElement);
+
+  effect = new T.StereoEffect(renderer);
+  effect.eyeSeparation = 1;
+  effect.focalLength = 25;
+  effect.setSize(core.options.width, core.options.height);
+
+  var textureLoader = new T.TextureLoader();
+  var groundTexture = textureLoader.load('grid.png');
+
+  groundTexture.wrapS = groundTexture.wrapT = T.RepeatWrapping;
+  groundTexture.repeat.set(256, 256);
+  groundTexture.anisotropy = renderer.getMaxAnisotropy();
+  ground = core.build(
+    'PlaneBufferGeometry', [2000, 2000, 100],
+    'MeshLambertMaterial', [{
+      color: 0xffffff,
+      map: groundTexture
+    }]
+  );
+
+  // lower the ground
+  ground.position.y = -10;
+  // make it flat
+  ground.rotation.x = -Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add(ground);
+
+  ambientLight = new T.AmbientLight(0xffffff, .8);
+  scene.add(ambientLight);
+
+  light = new T.DirectionalLight(0xffffff, 1);
+  light.color.setHSL(0.1, 1, 0.95);
+  light.position.set(0, 20, -10);
+  light.position.multiplyScalar(40);
+  light.castShadow = true;
+
+  // Increase size for sharper shadows
+  light.shadow.MapWidth = 1024;
+  light.shadow.MapHeight = 1024;
+
+  var d = 256;
+
+  light.shadow.camera.left = -d;
+  light.shadow.camera.right = d;
+  light.shadow.camera.top = d;
+  light.shadow.camera.bottom = -d;
+
+  light.shadow.camera.far = 3500;
+  light.shadow.bias = -0.0001;
+
+  scene.add(light);
+
+  var commandSheetTexture = textureLoader.load('commands_.png');
+  var commandSheet = core.build(
+    'BoxGeometry', [100, 50, 1],
+    'MeshLambertMaterial', [{
+      color: 0xffffff,
+      map: commandSheetTexture,
+    }]
+  );
+  commandSheet.position.z = -120;
+  commandSheet.position.y = 20;
+
+
+    var introButtonTexture = textureLoader.load('start.png');
+    var introButton = core.build(
+        'BoxGeometry', [32, 16, 1],
+        'MeshLambertMaterial', [{
+            color: 0x4c6ef5,
+            map: introButtonTexture
+        }]
+    );
+    introButton.position.z = 30;
+
+    introButton.ongazelong = function () {
+      init();
+      commandSheet.remove(introButton);
+      reticle.remove_collider(introButton);
+    }
+
+    reticle.add_collider(introButton);
+    commandSheet.add(introButton);
+
+  scene.add(commandSheet);
+
+
+  var moveAxis = function (obj, axis, amount) {
+    var position = {};
+    var position_to = {};
+    position[axis] = obj.position[axis];
+    position_to[axis] = obj.position[axis] += amount;
+
+    var obj_tween = new TWEEN.Tween(position).to(position_to, 500);
+    obj_tween.onUpdate(function() {
+        obj.position[axis] = position[axis];
+    });
+    obj_tween.start();
+
+  }
+  var rotateAxis = function (obj, axis, amount) {
+    var rotation = {};
+    var rotation_to = {};
+    rotation[axis] = obj.rotation[axis];
+    rotation_to[axis] = obj.rotation[axis] += amount;
+
+    var obj_tween = new TWEEN.Tween(rotation).to(rotation_to, 500);
+    obj_tween.onUpdate(function() {
+        obj.rotation[axis] = rotation[axis];
+    });
+    obj_tween.start();
+  }
+
+  //
+
+  var parseTranscriptAction = function(transcript, obj) {
+    var jokes = [
+      ["What do you call a laughing jar of mayonnaise?", "la-mayo"],
+      ["What kind of jokes do you make in the shower?", "Clean jokes!"],
+      ["What's the best thing about Switzerland?", "I don't know, but the flag is a big plus."],
+      ["Did you hear about the mathematician who's afraid of negative numbers?", "He'll stop at nothing to avoid them."],
+      ["Helvetica and Times New Roman walk into a bar.", "\"Get out of here!\" shouts the bartender. \"We don't serve your type.\""],
+      ["Why is it hard to make puns for kleptomaniacs?", "They are always taking things literally."],
+      ["How do you keep an idiot in suspense?"],
+      ["What do I look like? A JOKE MACHINE!?"],
+    ];
+
+    switch (transcript) {
+      case 'step forward':
+      case 'step forwards':
+      case 'go forward':
+      case 'go forwards':
+      case 'move forward':
+      case 'move forwards':
+      case 'forwards':
+      case 'forward':
+        console.log('Moving forward');
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance('Moving forward'));
+
+        moveAxis(obj, 'z', -10);
+        rotateAxis(obj, 'x', -5)
+      break;
+
+      case 'step back':
+      case 'step backward':
+      case 'step backwards':
+      case 'go back':
+      case 'go backward':
+      case 'go backwards':
+      case 'move back':
+      case 'move backward':
+      case 'move backwards':
+      case 'backwards':
+      case 'backward':
+      case 'back':
+        console.log('Moving backward');
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance('Moving backward'));
+
+        moveAxis(obj, 'z', 10);
+        rotateAxis(obj, 'x', 5);
+      break;
+
+      case 'step left':
+      case 'go left':
+      case 'move left':
+      case 'left':
+        console.log('Moving left');
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance('Moving left'));
+
+        moveAxis(obj, 'x', -10);
+        rotateAxis(obj, 'z', 5);
+      break;
+
+      case 'step right':
+      case 'go right':
+      case 'move right':
+      case 'right':
+        console.log('Moving right');
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance('Moving right'));
+
+        moveAxis(obj, 'x', 10);
+        rotateAxis(obj, 'z', -5);
+      break;
+
+      // Maybe when I understand Quaternions...
+
+      // case 'turn left':
+      // case 'rotate left':
+      // case 'spin left':
+      //   console.log('Turning left');
+      //   window.speechSynthesis.speak(new SpeechSynthesisUtterance('Turning left'));
+      //   rotateAxis(obj, 'y', 2.5);
+      // break;
+
+      // case 'turn right':
+      // case 'rotate right':
+      // case 'spin right':
+      //   console.log('Turning right');
+      //   window.speechSynthesis.speak(new SpeechSynthesisUtterance('Turning right'));
+      //   rotateAxis(obj, 'y', -2.5);
+      // break;
+
+      case 'tell me a joke':
+      case 'tell a joke':
+      case 'tell joke':
+      case 'tell me another joke':
+      case 'tell another joke':
+      case 'another joke':
+      case 'joke':
+      case 'jokes':
+        var index = Math.floor(Math.random() * jokes.length);
+        var joke = jokes[index];
+
+        joke.forEach(function(part, i) {
+          window.speechSynthesis.speak(new SpeechSynthesisUtterance(part))
+          setTimeout(function() {}, 250);
+        });
+      break;
+
+      case 'sing me a song':
+      case 'sing a song':
+      case 'sing song':
+      case 'song':
+      case 'sing':
+        var songs = [
+          [
+            "Daisy, Daisy give me your answer do.",
+            "I'm half crazy all for the love of you.",
+            "It won't be a stylish marriage,",
+            "I can't afford a carriage.",
+            "But you'll look sweet,",
+            "Upon the seat,",
+            "Of a bicycle made for two."
+          ],
+          [
+            "Hey I just met you",
+            "And this is crazy",
+            "But here's my number",
+            "So call me maybe",
+            "It's hard to look right at you baby",
+            "But here's my number",
+            "So call me maybe"
+          ]
+        ];
+
+        var index = Math.floor(Math.random() * songs.length);
+        var song = songs[index];
+        song.forEach(function(line, i) {
+          window.speechSynthesis.speak(new SpeechSynthesisUtterance(line))
+          setTimeout(function() {}, 250);
+        });
+        break;
+
+
+      default:
+        console.log("Sorry, I don't understand " + transcript);
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance("Sorry, I don't understand " + transcript));
+        break;
+    }
+  }
+
+  function startListening() {
+    var recognition = new(webkitSpeechRecognition || SpeechRecognition)();
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+    recognition.start();
+
+    recognition.onstart = function () {
+      micon.material.map = textureLoader.load('mic-on.png');
+      micon.scale.set(1,1,1);
+    };
+
+    recognition.onend = function() {
+      micon.material.map = textureLoader.load('mic-off.png');
+      micon.scale.set(.75,.75,.75);
+    };
+
+    recognition.onresult = function() {
+      parseTranscriptAction(event.results[0][0].transcript, obj);
+    };
+  };
+
+
+  var init = function() {
+
+    obj = core.build(
+      'SphereGeometry', [6, 32, 32],
+      'MeshLambertMaterial', [{
+        color: 0xffffff,
+        map: textureLoader.load('ball.png')
+      }]
+    );
+
+    obj.position.z = -20;
+    // obj.position.y = -4;
+    obj.position.y = 100;
+    scene.add(obj);
+    moveAxis(obj, 'y', -104);
+
+    obj.direction = new T.Vector3(0,0,0);
+
+    micon = core.build(
+        'CylinderGeometry', [1.8, 1.8, 0, 32],
+        'MeshLambertMaterial', [{
+            color: 0xffffff,
+            map: textureLoader.load('mic-off.png'),
+            transparent: true
+        }]
+    );
+    micon.rotation.y = Math.PI/2;
+    micon.rotation.x = Math.PI/2;
+    micon.position.z = -15;
+    // micon.position.x = 4;
+    micon.position.y = -6;
+    micon.scale.set(.75,.75,.75);
+
+    camera.add(micon);
+
+    document.body.addEventListener('click', startListening);
+  }
+
+
+
+  //
+
+
+
+  var animateRenderer = function() {
+    controls.update();
+    effect.render(scene, camera);
+    reticle.reticle_loop();
+    requestAnimationFrame(animateRenderer);
+    TWEEN.update();
+  };
+  animateRenderer();
+  window.addEventListener('resize', function() {
+    core.resizeRenderer(renderer, scene, camera, effect);
+  }, false);
+
+}(window, document));
