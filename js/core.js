054c4f0abf5cfd2b3e6c35b95e9a63d1f12fd0ea
Max Glenister <moglenstar@gmail.com>
2016-10-12 22:27:53 +0100

    Made ground global

--- js/core.js
+++ js/core.js
@@ -119,7 +119,7 @@ var core = function(window) {
     var groundTexture = textureLoader.load('../js/grid.png');
     groundTexture.wrapS = groundTexture.wrapT = T.RepeatWrapping;
     groundTexture.repeat.set(128, 128);
-    var ground = core.build(
+    window.ground = core.build(
       'PlaneBufferGeometry', [1000, 1000, 100],
       'MeshLambertMaterial', [{
         color: 0xffffff,
