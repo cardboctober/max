c745a3ffd0434ad5fe658736761e7756b63c793e
Max Glenister <moglenstar@gmail.com>
2016-10-29 23:49:29 +0100

    playing around with moving the piece based on look direction

--- 27/demo.js
+++ 27/demo.js
@@ -133,29 +133,28 @@
     ]
   }, ];
   var rows = 22;
-  var cols = 10;
+  var cols = 20;
   var board;
   var blockSize = 5.2;
   var shape;
-  var currentY = rows - 1;
+  var currentY = rows - 4;
   var middleCol = Math.floor(cols / 2);
   var currentX = middleCol;
   var gameover = false;
   var states = {
     EMPTY: 0,
     ACTIVE: 1,
-    FILLed: 2
+    FILLED: 2
   };
   var positions = [];
   var circle = {
     width: 20,
     depth: 20,
-    radius: 23
+    radius: 60
   };
   var step = (Math.PI * .8) / cols;
   var angle = Math.PI * 1.133;
   for (var c = 0; c < cols; c++) {
-    // put the calculated cell position in our array
     var position = new T.Vector3(
       (cols / circle.width) + (circle.radius * Math.cos(angle)),
       0,
@@ -164,7 +163,6 @@
     positions.push(position);
     angle += step;
   };
-  // console.log(positions);
   var defaultColor = 0xffffff;
   var makeBlock = function() {
     return core.build(
@@ -188,7 +186,7 @@
   var dropInterval = 1000;
   var lastTime = 0;
   var dropCounter = 0;
-  var deactivateBlocks = function(board) {
+  var deactivateBlocks = function() {
     board.forEach(function(row, r) {
       row.forEach(function(col, c) {
         if (col.state == states.ACTIVE) {
@@ -205,69 +203,83 @@
     if (dropCounter >= dropInterval) {
       if (currentY > shape.layout[0].length) {
         currentY--;
-        console.log(currentY);
       } else {
-        currentY = rows - 1;
+        currentY = rows - 4;
         newPiece();
       }
+
+      if (getLooking() === 'left') {
+        updateXPos(shape, -1);
+      }
+      if (getLooking() === 'right') {
+        updateXPos(shape, 1);
+      }
+
       dropCounter = 0;
     }
     lastTime = now;
     // reset all active's
-    deactivateBlocks(board);
+    deactivateBlocks();
+
+
+
+
     // draw the shape
     shape.layout[0].forEach(function(row, r) {
       row.forEach(function(col, c) {
         if (col > 0) {
           var x = currentX + c; // draw horizontally
-          var y = currentY - r; // draw vertically
+          var y = currentY + r; // draw vertically
           if (!collide(x, y, shape.layout[0])) {
-            if (typeof board[y] != 'undefined' &&
-              typeof board[y][x] != 'undefined'
-            ) {
-              board[y][x].state = states.ACTIVE;
-              board[y][x].material.color.setHex(shape.color);
-            }
+            board[y][x].state = states.ACTIVE;
+            board[y][x].material.color.setHex(shape.color);
           }
         }
       });
     });
   };
   var isActive = function(block) {
-    return block.state === states.ACTIVE;
+    return block.state === states.ACTIVE || block.state === states.FILLED;;
   }
   var collide = function(offsetX, offsetY, shape) {
     offsetX = offsetX || 0;
     offsetY = offsetY || 0;
-    offsetX = currentX + offsetX;
-    offsetY = currentY + offsetY;
-    // for (var y = 0; y < shape.length; y++) {
-    //     for (var x = 0; x < shape[0].length; x++) {
-    //         if (shape[y][x] !== 0) {
-    //             if (
-    //                 x + offsetX < 0 ||
-    //                 y + offsetY >= rows ||
-    //                 x + offsetX >= cols
-    //             ) {
-    //                 return true;
-    //             }
-    //             if (isActive(board[y + offsetY][x + offsetY])) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    // }
+    // console.log(offsetY, offsetX);
+    // offsetX = currentX + offsetX;
+    // offsetY = currentY + offsetY;
+
+    for (var y = 0; y < shape.length; y++) { // shape rows
+      for (var x = 0; x < shape[0].length; x++) { // shape cols
+        if (shape[y][x] !== 0) { // if it's not an empty block of the shape
+          if (
+            x + offsetX < 0 ||
+            x + offsetX >= cols ||
+            y + offsetY >= rows
+          ) { // if we're outside the board
+            return true
+          }
+          if (isActive(board[offsetY + y][offsetX + x])) {
+            board[offsetY + y][offsetX + x].state = states.FILLED;
+            return true;
+          }
+        }
+      }
+    }
     return false;
   };
-  var drawBoard = function(board) {
+  var drawBoard = function() {
     for (var r = 0; r < board.length; r++) {
       for (var c = 0; c < board[r].length; c++) {
         scene.remove(board[r][c]);
         // var x = c * blockSize - (blockSize * cols/2);
         var y = r * blockSize - (blockSize * 2);
         // var z = -40;
+        if (r < 1) {
+          board[r][c].state = states.FILLED;
+
+        }
         board[r][c].position.set(positions[c].x, y, positions[c].z);
-        board[r][c].material.opacity = isActive(board[r][c]) ? 1 : .2; // .2 for debug
+        board[r][c].material.opacity = isActive(board[r][c]) ? 1 : 0; // .2 for debug
         board[r][c].lookAt(new T.Vector3(core.center.x, y, core.center.z));
         board[r][c].castShadow = isActive(board[r][c]);
         board[r][c].recieveShadow = isActive(board[r][c]);
@@ -345,10 +357,27 @@
     newPiece();
     update();
   };
+  var getpos = function (obj) {
+    camera.matrixWorldInverse.getInverse( camera.matrixWorld );
+    var mat = new THREE.Matrix4().multiplyMatrices( camera.matrixWorldInverse, obj.matrixWorld );
+    var pos = obj.position.clone().applyProjection(mat);
+    return pos;
+  };
+  var getLooking = function () {
+    var pos = getpos(board[0][Math.floor(cols/2)]);
+
+    // MAGIC
+    if (pos.x - pos.z > 180) {
+      return 'left';
+    }
+    if (pos.x - pos.z < 120) {
+      return 'right';
+    }
+  };
   var update = function() {
     if (!gameover) {
       drawShape();
-      drawBoard(board);
+      drawBoard();
     } else {
       endGame();
     }
